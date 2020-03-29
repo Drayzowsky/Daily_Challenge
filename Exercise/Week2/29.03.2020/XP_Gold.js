424a0dab12a6f6e154cb0bdfa4b61e5dcab61bff
Drayzowsky <anthonydray@tutanota.com>
2020-03-29 22:28:20 +0300

    Week 2 started

---
+++ Exercise/Week2/29.03.2020/XP_Gold.js
@@ -0,0 +1,33 @@
+	// XP Gold 
+
+// Exercise 1
+
+Write a simple JavaScript program to join all elements of the following array into a string.
+
+var me = ["my","favorite","color","is","blue"]
+undefined
+var string= me.join(' ')
+undefined
+string
+"my favorite color is blue"
+
+// Exercise 2
+
+Create two strings,
+Create a variable which value is the concatenation of the two strings (separated by a space) slicing out and swapping the first 2 characters of each.
+
+let str1= "Hello "
+undefined
+let str2= "dude"
+undefined
+let str= str1+str2
+undefined
+str
+"Hello dude"
+let NewWord= str2.slice(0, 2) + str1.slice(2) + " " + str1.slice(0, 2) + str2.slice(2)
+undefined
+NewWord
+"dullo Hede"
+
+
+
---
+++ Exercise/Week2/29.03.2020/script.js
@@ -0,0 +1,10 @@
+
+let adressNumber="5";
+let adressStreet="Simtat Hahoma";
+let country="Israel";
+
+let global_adress=adressNumber+adressStreet+country;
+
+let birth_year="1994";
+let future_year="2025";
+let age= future_year - birth_year;
