2bd2212e50d647b4602a4acd7a85176352711d06
Drayzowsky <anthonydray@tutanota.com>
2020-04-19 18:49:03 +0300

    week4 started and XP added

---
+++ Exercise/Week4/19.04.20/xp.py
@@ -0,0 +1,33 @@
+# Exercise Xp
+
+# Exercise 1
+print("Hello World\n" * 4)
+
+# Exercise 2
+print((99 ^ 3) * 8)
+
+# Exercise 3
+name = "Anthony"
+age = 25
+shoe_size = "43"
+info = "My name is " + name + " I am " + str(age) + " and my shoes size is " + shoe_size
+print(info)
+
+# Exercise 4
+sentence = "I have a razer computer"
+computer_brand = "Apple"
+print(sentence.replace("razer", computer_brand))
+
+# Exercise 6
+user_height = int(input("What is your height?"))
+if user_height < 35:
+    print("Sorry, you can't ride today")
+else:
+    print("Let's go, enjoy!")
+
+# Exercise 7
+number = int(input("Choose a number"))
+if number % 2 == 0:
+    print("it's an even number")
+else:
+    print("it's an odd number")
